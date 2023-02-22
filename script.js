691a7f88d55507a1b5bd070c1b05aea6b73a9b50
Nicolas <nosoyna1des17@gmail.com>
2023-02-22 11:41:41 -0300

    Funcionalidades

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ var dos = document.getElementById("dos");
 var tres = document.getElementById("tres");
 var cuatro = document.getElementById("cuatro");
 var cinco = document.getElementById("cinco");
+/* ----------------------------------------- */
+var rating = undefined;
 
 uno.onclick = () => {
-  console.log("1");
+  rating = 1;
+  console.log(`El rating es: ${rating}`);
   uno.style.backgroundColor = "#fc7615";
   uno.style.color = "#feffff";
   dos.style.backgroundColor = "#232c35";
@@ -14,7 +17,8 @@ uno.onclick = () => {
   cinco.style.backgroundColor = "#232c35";
 };
 dos.onclick = () => {
-  console.log("2");
+  rating = 2;
+  console.log(`El rating es: ${rating}`);
   dos.style.backgroundColor = "#fc7615";
   dos.style.color = "#feffff";
   uno.style.backgroundColor = "#232c35";
@@ -23,7 +27,8 @@ dos.onclick = () => {
   cinco.style.backgroundColor = "#232c35";
 };
 tres.onclick = () => {
-  console.log("3");
+  rating = 3;
+  console.log(`El rating es: ${rating}`);
   tres.style.backgroundColor = "#fc7615";
   tres.style.color = "#feffff";
   dos.style.backgroundColor = "#232c35";
@@ -32,7 +37,8 @@ tres.onclick = () => {
   cinco.style.backgroundColor = "#232c35";
 };
 cuatro.onclick = () => {
-  console.log("4");
+  rating = 4;
+  console.log(`El rating es: ${rating}`);
   cuatro.style.backgroundColor = "#fc7615";
   cuatro.style.color = "#feffff";
   dos.style.backgroundColor = "#232c35";
@@ -41,7 +47,8 @@ cuatro.onclick = () => {
   cinco.style.backgroundColor = "#232c35";
 };
 cinco.onclick = () => {
-  console.log("5");
+  rating = 5;
+  console.log(`El rating es: ${rating}`);
   cinco.style.backgroundColor = "#fc7615";
   cinco.style.color = "#feffff";
   dos.style.backgroundColor = "#232c35";
